e38a217fca12610511c228e147c5a669f5dc455f
anyhowwws <101925287+anyhowwws@users.noreply.github.com>
2022-03-27 22:43:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,30 +28,27 @@ def index():
         Card=request.form.get("Supplementary card")
         purchase = float(Purchase)
         card = float(Card)
-        # model = load("/Users/hows/Documents/CART.joblib")
-        # pred = model.predict([[float(rates)]])
-        # PRED="$"+str(round(pred[0][0],2))
         
-        #model1 = load("CART")
-        #model2 = load("RandomForest")
-        #model3 = load("GB")
+        model1 = load("CART")
+        model2 = load("RandomForest")
+        model3 = load("GB")
         
-        #pred1 = model1.predict([[purchase,card]])
-        #pred2 = model2.predict([[purchase,card]])
-        #pred3 = model3.predict([[purchase,card]])
-        #Result1=str(pred1)
-        #Results2=str(pred2)
-        #Result3=str(pred3)
+        pred1 = model1.predict([[purchase,card]])
+        pred2 = model2.predict([[purchase,card]])
+        pred3 = model3.predict([[purchase,card]])
+        Result1=str(pred1[0][0])
+        Results2=str(pred2[0][0])
+        Result3=str(pred3[0][0])
 
         # return(render_template("/Users/hows/Documents/week2/templates/index.html",result="1",result2="2",result3="3"))
-        return(render_template("index.html",result="1",result2="2",result3="3"))
+        return(render_template("index.html",result=Result1,result2=Result2,result3=Result3))
             
     else:
         # defaultRate=""
         # defaultResults = ""
         # defaultResponse = "Enter an amount above for an estimation!"
         # return(render_template("/Users/hows/Documents/week2/templates/index.html",result="1",result2="2",result3="3"))
-        return(render_template("index.html",result="-",result2="-",result3="-"))
+        return(render_template("index.html",result="Enter your values above to start!",result2="",result3=""))
 
 
 if __name__=="__main__": #required if not in DEV env
